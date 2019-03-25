3f9898e77eda839a846afc5c34b3a33056029783
Erion Pali <eerian00@gmail.com>
2019-03-25 02:44:48 -0400

    Added addPhraseToDisplay
    Got the random phrase to split and display on console

--- js/Phrase.js
+++ js/Phrase.js
@@ -9,5 +9,35 @@ class Phrase {
     /**
      * Display phrase on game board
      */
-    addPhraseToDisplay() {}
+    addPhraseToDisplay() {
+        //get random phrase from Game object
+        let randomPhrase = game.getRandomPhrase();
+        //select main div
+        const mainDiv = document.querySelector(".main-container");
+        //create a NEW div
+        const div = document.createElement("div");
+        //create a ul
+        const ul = document.createElement("ul");
+        //append NEW div inside MAIN div
+        mainDiv.appendChild(div);
+        //append the ul inside NEW div
+        div.appendChild(ul);
+        //loop thru the random phrase
+        for (let i = 0; i < randomPhrase.length; i++) {
+            //create an li for each letter after splitting the phrase
+            const li = document.createElement("li");
+            li.textContent = randomPhrase.split("")[i];
+            //add 'hide' and 'letter' classes to the letters
+            li.className = "hide letter";
+            ul.appendChild(li);
+            //if there is a space in the phrase add class 'space' to the space li
+            if (randomPhrase.split("")[i] === " ") {
+                li.className = "space";
+            }
+        }
+        //add ID and Class to new div
+        div.id = "phrase";
+        div.className = "section";
+    }
+}
 }
--- js/app.js
+++ js/app.js
@@ -1,10 +1,5 @@
-const logPhrase = phrase => {
-    console.log(`Phrase - phrase: `, phrase);
-};
 const game = new Game();
 
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
+const randomPhrase = game.getRandomPhrase();
+const phrase = new Phrase(randomPhrase.phrase);
+phrase.addPhraseToDisplay();
