25b6354db3432f00759abbc9c5d36b895c0f7666
Erion Pali <eerian00@gmail.com>
2019-03-25 01:34:26 -0400

    app.js update
    Got Console.log to print random phrase from Game object

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-// const logPhrase = phrase => {
-//   console.log(`Phrase - phrase: `, phrase);
-// };
-// const game = new Game();
+const logPhrase = phrase => {
+    console.log(`Phrase - phrase: `, phrase);
+};
+const game = new Game();
 
-// logPhrase(game.getRandomPhrase());
-// logPhrase(game.getRandomPhrase());
-// logPhrase(game.getRandomPhrase());
-// logPhrase(game.getRandomPhrase());
-// logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
+logPhrase(game.getRandomPhrase());
